b81f78aef00c092ab9b307c405b9715cc8e30d50
syhao <767884144@qq.com>
2020-03-10 11:33:46 +0800

    阿里云

---
+++ hello-world01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+ <div id="app">
+  <h2>祝你生日快乐，瘦成此图，再次到达人生的巅峰</h2>
+  <img src="https://inline-syhao-file-190805.oss-cn-beijing.aliyuncs.com/avatar/12.jpg" width=400px height=500px><br/>
+ <h2>生日快乐</h2><br/>
+  </div>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
